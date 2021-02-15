ce7903b26d7e949451e54e35cc71fab3658a13f8
Tomas Bedrnik <tomas.bedrnik@socialbakers.com>
2021-02-15 15:43:53 +0100

    fetch first and then decide example

--- example.js
+++ example.js
@@ -4,25 +4,81 @@ const output = document.getElementById('output')
 const buttonWhole = document.getElementById('analyse-whole')
 const buttonFirst = document.getElementById('analyse-first')
 
-const onChangeUrl = (mediainfo, headers = {}) => {
-	output.value = `Fetching ${fileurl.value}`
-	fetch(fileurl.value, {headers})
-	.then(res => {
-    console.log({type: res.type, size: res.headers})
-    return res.arrayBuffer()
-  })
-	.then(_buffer => {
-		const buffer = new Uint8Array(_buffer)
-		const readChunk = (chunkSize, offset) => buffer.slice(offset, offset + chunkSize)
-		const getSize = () => buffer.byteLength
-		return mediainfo.analyzeData(getSize, readChunk)
-	})
-  .then((result) => {
-    output.value = result
-  })
-  .catch((error) => {
-    output.value = `An error occured:\n${error.stack}`
-  })
+const fetchHeaders = async (url) => {
+  const {headers} = await fetch(url, {method: 'HEAD'})
+  const contentLength = headers.get('content-length')
+  const contentType = headers.get('content-type')
+  const acceptRanges = headers.get('accept-ranges')
+  return {contentLength, contentType, acceptRanges}
+}
+
+const fetchBytes = async (url, headers = {}) => {
+  const res = await fetch(url, {headers})
+  const buffer = await res.arrayBuffer()
+	return new Uint8Array(buffer)
+}
+
+const delay = (time) => new Promise((res) => setTimeout(res, time))
+
+const onChangeUrl = async (mediainfo, firstBytesCount = 0) => {
+  try {
+    const URL = fileurl.value
+    const STR = {
+      url: URL,
+      status: `Fetching headers`
+    }
+    output.innerHTML = JSON.stringify(STR, null, 2)
+  
+    // Fetch headers first
+    const {headers: headers_in} = await fetch(URL, {method: 'HEAD'})
+    const contentLength = parseInt(headers_in.get('content-length'))
+    const contentType = headers_in.get('content-type')
+    const acceptRanges = headers_in.get('accept-ranges')
+    const analyseFirstBytes = acceptRanges === 'bytes' && firstBytesCount > 0
+
+    STR.headers = {contentLength, contentType, acceptRanges}
+    output.innerHTML = JSON.stringify(STR, null, 2)
+    
+    let needThoroughAnalysis = false
+    // Fetch first/all bytes
+    if (analyseFirstBytes) {
+      STR.status = `Fetching first ${firstBytesCount} bytes`
+      output.innerHTML = JSON.stringify(STR, null, 2)
+      const headers_out = {
+        range: `bytes=0-${firstBytesCount - 1}`
+      }
+      const buffer1 = await fetchBytes(URL, headers_out)
+      // Methods for mediainfo
+      const readChunk = (chunkSize, offset) => buffer1.slice(offset, offset + chunkSize)
+      const getSize = () => contentLength || buffer1.byteLength
+      // Perform analysis
+      const analysis1 = await mediainfo.analyzeData(getSize, readChunk)
+      STR.analysis_short = JSON.parse(analysis1)
+      output.innerHTML = JSON.stringify(STR, null, 2)
+ 
+      // TODO: if analysis1 doesn't contain all neccessary info and we analysed
+      // only first bytes we can perform additional analysis on whole content
+      await delay(1000) // To see the result before prompting
+      needThoroughAnalysis = !!prompt('Perform full analysis? (Cancel for no)', 'Yes please')
+    }
+
+    if (!analyseFirstBytes || needThoroughAnalysis) {
+      STR.status = `Fetching all bytes`
+      output.innerHTML = JSON.stringify(STR, null, 2)
+  
+      const buffer2 = await fetchBytes(URL)
+      // Methods for mediainfo
+      const readChunk = (chunkSize, offset) => buffer2.slice(offset, offset + chunkSize)
+      const getSize = () => contentLength || buffer2.byteLength
+      // Perform analysis
+      const analysis2 = await mediainfo.analyzeData(getSize, readChunk)
+      STR.analysis_full = JSON.parse(analysis2)
+      output.innerHTML = JSON.stringify(STR, null, 2)
+    }
+
+  } catch (error) {
+    output.innerHTML = `An error occured:\n${error.stack}`
+  }
 }
 
 const onChangeFile = (mediainfo) => {
@@ -59,5 +115,5 @@ const onChangeFile = (mediainfo) => {
 MediaInfo({ format: 'JSON' }, (mediainfo) => {
   fileinput.addEventListener('change', () => onChangeFile(mediainfo))
 	buttonWhole.addEventListener('click', () => onChangeUrl(mediainfo))
-	buttonFirst.addEventListener('click', () => onChangeUrl(mediainfo, {Range: 'bytes=0-10000'}))
+	buttonFirst.addEventListener('click', () => onChangeUrl(mediainfo, 10000))
 })
